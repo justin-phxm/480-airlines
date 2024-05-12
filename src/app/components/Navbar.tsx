e640370ba4142899282eabdc445f0d20f451294e
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-05-12 15:23:16 -0600

    chore: Update Hero and Navbar components styling

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   ];
   return (
     <>
-      <nav className="border-gray-200 bg-white dark:bg-gray-900">
+      <nav className="border-gray-200 bg-slate-50 dark:bg-gray-900">
         <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
           <Link
             href="/"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import Hero from "@/components/Hero";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-t from-indigo-300 to-white text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 ">
-        <Hero />
-      </div>
+    <main className="flex min-h-screen w-full flex-col items-center bg-gradient-to-t from-indigo-300 to-white text-white">
+      <Hero />
     </main>
   );
 }
