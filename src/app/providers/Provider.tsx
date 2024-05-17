5080b8c0fd28ece13a0e13b491658999ee28804e
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-05-17 00:28:42 -0600

    refactor: Remove unused code in Provider component

--- src/app/providers/Provider.tsx
+++ src/app/providers/Provider.tsx
@@ -1,15 +1,8 @@
 "use client";
-import { useEffect } from "react";
+
 import { ToastContainer } from "react-toastify";
-import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  useEffect(() => {
-    toast.success("Welcome to the app!", {
-      position: "bottom-right",
-    });
-  }, []);
-
   return (
     <>
       {children}
