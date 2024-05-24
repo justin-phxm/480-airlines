25cb0b61ee1bd1a6bbb66f7c13ea959b61e013bc
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-05-24 11:42:27 -0600

    chore: Refactor middleware to use withAuth and update role-based route protection logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,35 @@
 // import { Role } from "@prisma/client";
 // import { withAuth } from "next-auth/middleware";
 
-/* default protects routes by checking if user is logged in*/
-export { default } from "next-auth/middleware";
+import { Role } from "@prisma/client";
+import withAuth, { type NextRequestWithAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
 export const config = {
   matcher: ["/admin", "/employee", "/profile"],
 };
 
 /* Protect routes based on role */
-// export default withAuth({
-//   callbacks: {
-//     authorized: ({ req, token }) => {
-//       const { pathname } = req.nextUrl;
-//       if (token) {
-//         const { role } = token;
-//         if (pathname === "/admin") {
-//           return role === Role.ADMIN;
-//         }
-//         if (pathname === "/employee") {
-//           return role === Role.EMPLOYEE || role === Role.ADMIN;
-//         }
-//       }
-//       return Boolean(token);
-//     },
-//   },
-// });
+export default withAuth(
+  function middleware(request: NextRequestWithAuth) {
+    const { token } = request.nextauth;
+    if (token) {
+      const { role } = token;
+      const { pathname } = request.nextUrl;
+      if (pathname.startsWith("/admin")) {
+        if (role !== Role.ADMIN)
+          return NextResponse.rewrite(new URL("/denied", request.url));
+      }
+      if (pathname.startsWith("/employee")) {
+        if (role !== Role.EMPLOYEE && role !== Role.ADMIN) {
+          return NextResponse.rewrite(new URL("/denied", request.url));
+        }
+      }
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  },
+);
