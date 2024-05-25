fbe29ea9af79b165a7d4ae5b81c0fc3007221639
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-05-25 00:43:08 -0600

    add trending flights component to admin

---
+++ src/app/(protected)/admin/components/TrendingFlights.tsx
@@ -0,0 +1,113 @@
+import Image from "next/image";
+import React from "react";
+
+function AvatarGroup() {
+  const users = [
+    "https://via.placeholder.com/28x41",
+    "https://via.placeholder.com/31x46",
+    "https://via.placeholder.com/31x38",
+  ];
+  return (
+    <div className="flex w-full items-center justify-start gap-2">
+      {users.map((user, index) => (
+        <div
+          key={index}
+          className="flex size-7 items-center justify-center rounded-full bg-indigo-600"
+        >
+          <Image
+            className="rounded-full"
+            src={user}
+            alt="User"
+            width={28}
+            height={28}
+          />
+        </div>
+      ))}
+      <div className="flex size-7 items-center justify-center rounded-full bg-indigo-600">
+        <div className="flex h-7 w-7 items-center justify-center rounded-full border-2 border-white bg-slate-200">
+          <span className="text-xs font-bold text-indigo-600">18+</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+interface CardProps {
+  imageSrc: string;
+  title: string;
+  creator: string;
+  currentBid: string;
+}
+
+function Card({ imageSrc, title, creator, currentBid }: CardProps) {
+  return (
+    <div className="flex w-80 flex-col justify-between rounded-2xl bg-white p-4">
+      <div className=" flex h-52 w-full items-center justify-center rounded-2xl">
+        <Image
+          className="size-full rounded-2xl"
+          src={imageSrc}
+          alt={title}
+          width={208}
+          height={126}
+        />
+      </div>
+      <div className="mb-4 w-full">
+        <div className="text-lg font-bold leading-loose text-blue-950">
+          {title}
+        </div>
+        <div className="text-sm font-normal leading-tight text-slate-400">
+          By {creator}
+        </div>
+      </div>
+      <div className="mb-4 flex w-full items-center justify-between">
+        <div className="text-sm font-bold leading-normal text-indigo-600">
+          Current Bid: {currentBid}
+        </div>
+        <div className="flex h-8 w-28 items-center justify-center rounded-3xl bg-violet-950">
+          <span className="text-sm font-medium text-white">Place Bid</span>
+        </div>
+      </div>
+      <AvatarGroup />
+    </div>
+  );
+}
+
+const TrendingFlights: React.FC = () => {
+  const flights = [
+    {
+      imageSrc: "https://via.placeholder.com/396x238",
+      title: "Abstract Colors",
+      creator: "Esthera Jackson",
+      currentBid: "0.91 ETH",
+    },
+    {
+      imageSrc: "https://via.placeholder.com/782x594",
+      title: "ETH AI Brain",
+      creator: "Nick Wilson",
+      currentBid: "2.82 ETH",
+    },
+    {
+      imageSrc: "https://via.placeholder.com/358x446",
+      title: "Mesh Gradients",
+      creator: "Will Smith",
+      currentBid: "0.56 ETH",
+    },
+  ];
+  return (
+    <div className=" flex w-full flex-col items-start justify-start">
+      <div className="text-2xl font-bold">Trending Flights</div>
+      <div className="flex w-full items-end justify-start gap-5">
+        {flights.map((flight, index) => (
+          <Card
+            key={index}
+            imageSrc={flight.imageSrc}
+            title={flight.title}
+            creator={flight.creator}
+            currentBid={flight.currentBid}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TrendingFlights;
--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -1,11 +1,14 @@
 import History from "./components/History";
 import TopMembers from "./components/TopMembers";
+import TrendingFlights from "./components/TrendingFlights";
 
 export default function page() {
   return (
     <section className="flex flex-1 bg-gradient-to-t from-indigo-300 to-white p-4">
       <div className=" grid w-full grid-cols-3">
-        <div className=" col-span-2 bg-red-500">Admin View</div>
+        <div className=" col-span-2 bg-red-500">
+          <TrendingFlights />
+        </div>
         <div className=" col-span-1 bg-blue-500">
           <TopMembers />
           <History />
