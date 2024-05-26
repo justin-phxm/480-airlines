63f17bc54e1c252af1f17a5ba6d8b293300001b1
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-05-26 12:00:28 -0600

    chore: Add lazy loading for admin page components

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -1,19 +1,29 @@
+import { Suspense } from "react";
 import CreateNewEntityForm from "./components/CreateNewEntityForm";
 import History from "./components/History";
 import TopMembers from "./components/TopMembers";
 import TrendingFlights from "./components/TrendingFlights";
+import { Skeleton } from "@mui/material";
 
 export default function page() {
   return (
     <section className="flex flex-1 bg-gradient-to-t from-indigo-300 to-white p-4">
       <div className=" grid w-full grid-cols-3 gap-4">
         <div className=" col-span-2 flex flex-col gap-4">
-          <CreateNewEntityForm />
-          <TrendingFlights />
+          <Suspense fallback={<Skeleton />}>
+            <CreateNewEntityForm />
+          </Suspense>
+          <Suspense fallback={<Skeleton />}>
+            <TrendingFlights />
+          </Suspense>
         </div>
-        <div className=" col-span-1 flex flex-col gap-4">
-          <TopMembers />
-          <History />
+        <div className=" col-span-1 grid grid-flow-row grid-rows-2 flex-col gap-4">
+          <Suspense fallback={<Skeleton />}>
+            <TopMembers />
+          </Suspense>
+          <Suspense fallback={<Skeleton />}>
+            <History />
+          </Suspense>
         </div>
       </div>
     </section>
