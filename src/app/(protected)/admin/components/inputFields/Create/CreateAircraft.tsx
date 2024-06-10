403af50900d36efba0c3f67daefffc974a471dee
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-06-10 16:00:52 -0600

    chore: Remove unused input fields and update styling in CreateAircraft component

--- src/app/(protected)/admin/components/inputFields/Create/CreateAircraft.tsx
+++ src/app/(protected)/admin/components/inputFields/Create/CreateAircraft.tsx
@@ -11,7 +11,6 @@ export default function CreateAircraft() {
       <div className=" col-span-2 grid gap-2">
         <TextField
           className="w-full"
-          id="outlined-basic"
           label="Aircraft Name"
           variant="outlined"
           name="aircraftName"
@@ -19,7 +18,6 @@ export default function CreateAircraft() {
 
         <TextField
           className="w-full"
-          id="outlined-basic"
           label="# of Business Class Seat Rows"
           type="number"
           variant="outlined"
@@ -29,14 +27,12 @@ export default function CreateAircraft() {
       <div className="col-span-2 grid gap-2">
         <TextField
           className="w-full"
-          id="outlined-basic"
           label="# of First Class Seats Rows"
           variant="outlined"
           type="number"
           name="firstClassSeats"
         />
         <TextField
-          id="outlined-basic"
           type="number"
           className="w-full"
           label="# of Economy Class Seats Rows"
--- src/app/components/RandomAirlineImage.tsx
+++ src/app/components/RandomAirlineImage.tsx
@@ -38,6 +38,7 @@ export default function RandomAirlineImage({
             className={"object-contain " + className}
             placeholder="blur"
             fill
+            sizes="10vw"
           />
         </div>
       ) : (
