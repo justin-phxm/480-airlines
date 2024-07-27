04189bfeb04f8893b628f7427cd2401f21ef769b
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-07-27 16:29:19 -0600

    Add hamburger menu for navbar #8

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,9 @@
+"use client";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 import Link from "next/link";
 import LoginStatus from "./LoginStatus";
+import { useState } from "react";
 export default function Navbar() {
   const routes = [
     { name: "Home", href: "/" },
@@ -7,6 +11,7 @@ export default function Navbar() {
     { name: "Airline Employee", href: "/employee" },
     { name: "Membership", href: "/membership" },
   ];
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <nav className="border-gray-200 bg-slate-50 dark:bg-gray-900">
@@ -17,7 +22,7 @@ export default function Navbar() {
           >
             480 Airlines
           </Link>
-          <ul className="flex flex-col items-center font-medium md:flex-row md:space-x-8 rtl:space-x-reverse dark:text-white">
+          <ul className="hidden flex-col items-center font-medium md:flex-row md:space-x-8 lg:flex rtl:space-x-reverse dark:text-white">
             {routes.map((route) => (
               <li key={route.name} className=" hover:text-indigo-500">
                 <Link href={route.href}>{route.name}</Link>
@@ -25,7 +30,23 @@ export default function Navbar() {
             ))}
             <LoginStatus />
           </ul>
+          <div className="flex lg:hidden">
+            <GiHamburgerMenu size={24} onClick={() => setIsOpen(!isOpen)} />
+          </div>
         </div>
+        {isOpen && (
+          <ul className="flex flex-col items-end space-y-4 px-4 pb-4 font-medium lg:hidden ">
+            {routes.map((route) => (
+              <li
+                key={route.name}
+                className=" w-full border-b border-slate-500/50 text-end hover:text-indigo-500"
+              >
+                <Link href={route.href}>{route.name}</Link>
+              </li>
+            ))}
+            <LoginStatus />
+          </ul>
+        )}
       </nav>
     </>
   );
